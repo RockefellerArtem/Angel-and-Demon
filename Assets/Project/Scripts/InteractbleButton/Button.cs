59f19a2be21c006cd378fee5afb335bc470b23e3
Leontjev Michael <michlea@yandex.ru>
2023-04-07 21:09:43 +0400

    [develop] feat: add button down move

--- Assets/Project/Scripts/InteractbleButton/Button.cs
+++ Assets/Project/Scripts/InteractbleButton/Button.cs
@@ -1,18 +1,50 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    [SerializeField] float _buttonDownDelta;
+    [SerializeField] float _timeDown;
+
     public event Action OnButtonActivate;
     public event Action OnButtonDisable;
 
+    private bool _isMoving = false;
+    private Vector3 _downPosition, _upPosition;
+
+    public void OnEnable()
+    {
+        _downPosition = transform.position - new Vector3(0f, _buttonDownDelta, 0f);
+        _upPosition = transform.position;
+
+        OnButtonActivate += () => { StartCoroutine(Move(_upPosition, _downPosition)); };
+        OnButtonDisable += () => { StartCoroutine(Move(_downPosition, _upPosition)); };
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        if (_isMoving) return;
         OnButtonActivate?.Invoke();
     }
 
     public void OnCollisionExit2D(Collision2D collision)
     {
+        if (_isMoving) return;
         OnButtonDisable?.Invoke();
+    }
+
+    public IEnumerator Move(Vector3 startPos, Vector3 endPos)
+    {
+        _isMoving = true;
+        float t = 0f;
+        while (t <= _timeDown)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, t / _timeDown);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        _isMoving = false;
     }
 }
--- Assets/Project/Scripts/InteractbleButton/ConnectingObject.cs
+++ Assets/Project/Scripts/InteractbleButton/ConnectingObject.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ConnectingObject : MonoBehaviour
@@ -7,30 +8,35 @@ public class ConnectingObject : MonoBehaviour
     [SerializeField] float _height;
     [SerializeField] float _time;
 
+    private float _yOpen, _yClose;
+
     public void OnEnable()
     {
+        _yOpen = transform.localScale.y;
+        _yClose = transform.localScale.y + _height;
+
         _button.OnButtonActivate += OnButtonActivate;
         _button.OnButtonDisable += OnButtonDisable;
     }
 
     private void OnButtonDisable()
     {
-        StartCoroutine(ScaleY(-_height));
+        StartCoroutine(ScaleY(_yClose, _yOpen));
     }
 
     public void OnButtonActivate()
     {
-        StartCoroutine(ScaleY(_height));
+        StartCoroutine(ScaleY(_yOpen, _yClose));
     }
 
-    public IEnumerator ScaleY(float coeff)
+    public IEnumerator ScaleY(float startY, float endY)
     {
         float t = 0f;
         while(t <= _time)
         {
             var scale = transform.localScale;
             var position = transform.localPosition;
-            scale.y += coeff / _time * Time.deltaTime;
+            scale.y = Mathf.Lerp(startY, endY, t / _time);
             t += Time.deltaTime;
             transform.localScale = scale;
             position.y = -scale.y / 2;
