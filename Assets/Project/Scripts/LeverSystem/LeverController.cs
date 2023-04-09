f234b37758429b0d40e2cca5c1398c514f07aa28
Alexander Shushnov <Alexander.shushnov@yandex.ru>
2023-04-09 22:19:20 +0700

    [develop] fixed door animation

--- Assets/Project/Scripts/LeverSystem/LeverController.cs
+++ Assets/Project/Scripts/LeverSystem/LeverController.cs
@@ -7,6 +7,7 @@ public class LeverController : MonoBehaviour
     public float _rotationAngle;
 
     private Animator _doorAnim;
+    private int k = 0;
 
     private void Start()
     {
@@ -16,14 +17,16 @@ public class LeverController : MonoBehaviour
     private void Update()
     {
         float angle = _hingeJoint.jointAngle;
-        if (angle > _rotationAngle)
+        if (angle > _rotationAngle && k == 0)
         {
+            k = 1;
             Collider2D colliderToDisable = _door.GetComponent<Collider2D>();
             colliderToDisable.enabled = false;
             _doorAnim.SetTrigger("Play");
         }
-        else if (angle < -_rotationAngle)
+        else if (angle < -_rotationAngle && k == 1)
         {
+            k = 0;
             Collider2D colliderToDisable = _door.GetComponent<Collider2D>();
             colliderToDisable.enabled = true;
             _doorAnim.SetTrigger("Play2");
