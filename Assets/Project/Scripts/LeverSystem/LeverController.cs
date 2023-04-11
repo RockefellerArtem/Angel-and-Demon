2be5299d4733018f4adf452975531fbd15e52e96
Alexander Shushnov <Alexander.shushnov@yandex.ru>
2023-04-12 01:38:16 +0700

    [develop] add a test level and make small adjustments

--- Assets/Project/Scripts/LeverSystem/LeverController.cs
+++ Assets/Project/Scripts/LeverSystem/LeverController.cs
@@ -5,28 +5,30 @@ public class LeverController : MonoBehaviour
     public GameObject _door;
     public HingeJoint2D _hingeJoint;
     public float _rotationAngle;
+    public bool _invert;
 
     private Animator _doorAnim;
-    private int k = 0;
-
+    private int chet = 0;
+    private int invert = 1;
     private void Start()
     {
+        if (_invert == true) { invert = -1; }
         _doorAnim = _door.GetComponent<Animator>();
     }
 
     private void Update()
     {
         float angle = _hingeJoint.jointAngle;
-        if (angle > _rotationAngle && k == 0)
+        if (invert * angle > _rotationAngle && chet == 0)
         {
-            k = 1;
+            chet = 1;
             Collider2D colliderToDisable = _door.GetComponent<Collider2D>();
             colliderToDisable.enabled = false;
             _doorAnim.SetTrigger("Play");
         }
-        else if (angle < -_rotationAngle && k == 1)
+        else if (invert * angle < -_rotationAngle && chet == 1)
         {
-            k = 0;
+            chet = 0;
             Collider2D colliderToDisable = _door.GetComponent<Collider2D>();
             colliderToDisable.enabled = true;
             _doorAnim.SetTrigger("Play2");
--- Assets/Project/Scripts/WaterSystem/Movement.cs
+++ Assets/Project/Scripts/WaterSystem/Movement.cs
@@ -1,39 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10f;          // Скорость передвижения игрока
-    public float jumpForce = 500f;     // Сила прыжка
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private float moveInput;
 
     private Rigidbody2D rb;
-    private bool isGrounded = false;
+
+    private bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
 
     void Start()
     {
+        extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
-        Vector2 movement = new Vector2(moveHorizontal * speed, rb.velocity.y);
-        rb.velocity = movement;
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if (facingRight == false && moveInput > 0)
         {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            isGrounded = false;
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
         }
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    void Update()
     {
-        if (col.gameObject.tag == "Ground")
+        if (isGrounded == true)
         {
-            isGrounded = true;
+            extraJumps = extraJumpsValue;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
