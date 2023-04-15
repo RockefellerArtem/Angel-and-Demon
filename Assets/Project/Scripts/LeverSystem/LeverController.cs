1ad48f9d8b940c2498be0a16a9ddca75b150df46
Alexander Shushnov <Alexander.shushnov@yandex.ru>
2023-04-15 23:37:34 +0700

    [develop] fixed lever script

--- Assets/Project/Scripts/LeverSystem/LeverController.cs
+++ Assets/Project/Scripts/LeverSystem/LeverController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class LeverController : MonoBehaviour
 {
-    public GameObject _door;
+    public GameObject _doorOrFan;
     public HingeJoint2D _hingeJoint;
     public float _rotationAngle;
     public bool _invert;
+    public bool _door;
+    public bool _fan;
 
     private Animator _doorAnim;
     private int chet = 0;
@@ -13,25 +15,46 @@ public class LeverController : MonoBehaviour
     private void Start()
     {
         if (_invert == true) { invert = -1; }
-        _doorAnim = _door.GetComponent<Animator>();
+        if (_door == true) { 
+        _doorAnim = _doorOrFan.GetComponent<Animator>();
+            }
     }
 
     private void Update()
     {
         float angle = _hingeJoint.jointAngle;
-        if (invert * angle > _rotationAngle && chet == 0)
+        if (_door == true)
         {
-            chet = 1;
-            Collider2D colliderToDisable = _door.GetComponent<Collider2D>();
-            colliderToDisable.enabled = false;
-            _doorAnim.SetTrigger("Play");
+            if (invert * angle > _rotationAngle && chet == 0)
+            {
+                chet = 1;
+                Collider2D colliderToDisable = _doorOrFan.GetComponent<Collider2D>();
+                colliderToDisable.enabled = false;
+                _doorAnim.SetTrigger("Play");
+            }
+
+            else if (invert * angle < -_rotationAngle && chet == 1)
+            {
+                chet = 0;
+                Collider2D colliderToDisable = _doorOrFan.GetComponent<Collider2D>();
+                colliderToDisable.enabled = true;
+                _doorAnim.SetTrigger("Play2");
+            }
         }
-        else if (invert * angle < -_rotationAngle && chet == 1)
+        else if (_fan == true)
         {
-            chet = 0;
-            Collider2D colliderToDisable = _door.GetComponent<Collider2D>();
-            colliderToDisable.enabled = true;
-            _doorAnim.SetTrigger("Play2");
+            if (invert * angle > _rotationAngle && chet == 0)
+            {
+                chet = 1;
+                _doorOrFan.GetComponent <WindGenerator>().TurnOn();
+            }
+
+            else if (invert * angle < -_rotationAngle && chet == 1)
+            {
+                chet = 0;
+                _doorOrFan.GetComponent<WindGenerator>().TurnOff();
+            }
         }
+        
     }
 }
