007ec617363c85d9f0e6d6b4614f3d4c1e8f669a
Artem Filippov <90859140+RockefellerArtem@users.noreply.github.com>
2023-04-20 23:26:34 +0300

    [Project] [develop] Fix machanics open/close wall

--- Assets/Project/Scripts/DoorSystem/DoorEngine.cs
+++ Assets/Project/Scripts/DoorSystem/DoorEngine.cs
@@ -21,15 +21,17 @@ public class DoorEngine : MonoBehaviour
         
         if (isState)
         {
+            _animatorDoor.ResetTrigger("Down");
             _animatorDoor.SetTrigger("Up");
+            
             _animatorPlatform.ResetTrigger("Up");
             _animatorPlatform.SetTrigger("Lower");
         }
         else
         {
+            _animatorDoor.ResetTrigger("Up");
             _animatorDoor.SetTrigger("Down");
-            _animatorPlatform.ResetTrigger("Up");
-            _animatorDoor.SetTrigger("Up");
+            
             _animatorPlatform.ResetTrigger("Lower");
             _animatorPlatform.SetTrigger("Up");
             
